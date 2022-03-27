030c4274d26448270bd80fb83c97cf53606500af
tcpdumping <56612478+tcpdumping@users.noreply.github.com>
2022-03-26 19:59:09 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,8 +1,9 @@
 // ACE-IAC Git Aviatrix Infrastructure
 
-data "aviatrix_gateway" "egress" {
-  gw_name = var.azure_spoke2_name
-}
+#data "aviatrix_gateway" "egress" {
+#  gw_name = var.azure_spoke2_name
+#}
+egress = "ace-iac-spoke2"
 
 resource "aviatrix_fqdn" "fqdn_filter" {
   fqdn_tag     = "APP-RULES"
